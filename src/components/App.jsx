95c35f090c69835ea37b2b6ba71fd500eb2253c3
Kushnir88 <mr.kushnir.igor@gmail.com>
2023-07-29 22:41:50 +0300

    begin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+const Home = React.lazy(() => import('../components/Home'));
+const MovieDetails = React.lazy(() => import('../components/MovieDetails'));
+const Cast = React.lazy(() => import('../components/Cast'));
+const Reviews = React.lazy(() => import('../components/Reviews'));
+const SearchPage = React.lazy(() => import('../pages/SearchPage'));
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Пошук</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies/:movieId" element={<MovieDetails />} />
+            <Route path="/movies/:movieId/cast" element={<Cast />} />
+            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+            <Route path="/search" element={<SearchPage />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
   );
 };
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+// index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import ReactDOM from 'react-dom';
+import App from './components/App';
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
